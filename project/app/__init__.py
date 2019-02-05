54615ed776f7bf6ba47494075325e713f0a37003
sr9yar <340999@gmail.com>
2019-02-05 14:04:17 +0300

    working now

--- project/app/__init__.py
+++ project/app/__init__.py
@@ -3,9 +3,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-  return "Hello World 112234"
+  return "Hello World 0000     112234"
 
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+  app.run( 
+      host='0.0.0.0', 
+      port=5000
+    )
